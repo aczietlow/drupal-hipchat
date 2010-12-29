cd66dc762ae88f65dc3ea1444a3296dd7c45255a
Morbus Iff <morbus@disobey.com>
2010-12-29 14:42:33 +0000

    All init stuff moved to bot.smartirc.inc to prep for Drush patch.

--- bot_start.php
+++ bot_start.php
@@ -3,9 +3,7 @@
 
 /*
  * @file
- * Wrapper around the Drupal bootstrap and Net_SmartIRC. The first half is
- * devoted to fooling Drupal into thinking it's being run through the web,
- * and the other half inserts the IRC library into Drupal's hook system.
+ * Simple command-line startup script for the bot.
  */
 
 set_time_limit(0); // ignore max_execution_time.
@@ -37,52 +35,9 @@ while ($param = array_shift($_SERVER['argv'])) {
 }
 
 // load in required libraries.
-require_once 'Net/SmartIRC.php';
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL); // gotta bootstrap everything in before d_g_p.
 require_once DRUPAL_ROOT . '/' . drupal_get_path('module', 'bot') . '/bot.smartirc.inc';
 
-// prevent MySQL timeouts on slow channels.
-db_query('SET SESSION wait_timeout = 86400');
-
-// initialize the bot with some sane defaults.
-global $irc; // allow it to be slurped by Drupal modules if need be.
-$irc = new Net_SmartIRC(); // MmmmmmM. The IRC object itself. Magick happens here.
-$irc->setDebug( variable_get('bot_debugging', 0) ? SMARTIRC_DEBUG_ALL : SMARTIRC_DEBUG_NONE );
-// the (boolean) here is required, as Net_SmartIRC doesn't respect a FAPI checkbox value of 1, only TRUE.
-$irc->setAutoReconnect((boolean) variable_get('bot_auto_reconnect', 1)); // reconnect to the server if disconnected.
-$irc->setAutoRetry((boolean) variable_get('bot_auto_retry', 1)); // retry if a server connection fails.
-$irc->setUseSockets((boolean) variable_get('bot_real_sockets', 1)); // socket_connect or fsockopen?
-$irc->setChannelSyncing(TRUE); // keep a list of joined users per channel.
-
-// send every message type the library supports to our wrapper class.
-// we can automate the creation of these actionhandlers, but not the
-// class methods below (only PHP 5 supports default methods easily).
-$irc_message_types = array(
-  'UNKNOWN',    'CHANNEL', 'QUERY',    'CTCP',        'NOTICE',       'WHO',
-  'JOIN',       'INVITE',  'ACTION',   'TOPICCHANGE', 'NICKCHANGE',   'KICK',
-  'QUIT',       'LOGIN',   'INFO',     'LIST',        'NAME',         'MOTD',
-  'MODECHANGE', 'PART',    'ERROR',    'BANLIST',     'TOPIC',        'NONRELEVANT',
-  'WHOIS',      'WHOWAS',  'USERMODE', 'CHANNELMODE', 'CTCP_REQUEST', 'CTCP_REPLY',
-);
-
-foreach ($irc_message_types as $irc_message_type) {
-  $class = 'bot_irc_msg_' . drupal_strtolower($irc_message_type);
-  $irc->registerActionhandler(constant('SMARTIRC_TYPE_' . $irc_message_type), '.*', new $class(), 'invoke');
-}
-
-// set up a timers similar to Drupal's hook_cron(), multiple types. I would have
-// liked to just pass a parameter to a single function, but SmartIRC can't do that.
-$irc->registerTimehandler(300000, new bot_irc_bot_cron(),         'invoke'); // 5 minutes.
-$irc->registerTimehandler(60000,  new bot_irc_bot_cron_faster(),  'invoke'); // 1 minute.
-$irc->registerTimehandler(15000,  new bot_irc_bot_cron_fastest(), 'invoke'); // 15 seconds.
-
-// connect and begin listening.
-$irc->connect(variable_get('bot_server', 'irc.freenode.net'), variable_get('bot_server_port', 6667));
-$irc->login(variable_get('bot_nickname', 'bot_module'), variable_get('bot_nickname', 'bot_module') . ' :http://drupal.org/project/bot', 8, variable_get('bot_nickname', 'bot_module'), variable_get('bot_password'));
-
-// channel joining has moved to bot_irc_bot_cron_fastest().
-// read that function for the rationale, and what we gain from it.
-
-$irc->listen(); // go into the forever loop - no code after this is run.
-$irc->disconnect(); // if we stop listening, disconnect properly.
+// start the shindig.
+bot_smartirc_start();
